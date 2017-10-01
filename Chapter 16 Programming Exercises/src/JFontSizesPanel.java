ee54fbcc4ce1fe087601410fd482a71d81e0dcd2
BenjaminHerd <benoherdo@gmail.com>
2017-10-01 12:18:23 +1000

    JFontSizesPanel.java exercise completed (Drawing strings, Graphics class mess around)

---
+++ Chapter 16 Programming Exercises/src/JFontSizesPanel.java
@@ -0,0 +1,48 @@
+/*
+
+ */
+
+//import statements
+import javax.swing.*;
+import java.awt.*;
+
+//main class constructor
+public class JFontSizesPanel extends JPanel{
+
+    //variable declaration and layout components
+    String message = "Text to be displayed";
+    final int START = 6;
+    final int STOP = 20;
+    final int HORIZ_POS = 50;
+    int vertPos;
+    int i;
+    final int INCREASE = 20;
+    Font myFont;
+
+    //Override functions
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        vertPos = 10;
+
+        //For loop for drawing a string and then
+        // moving position and increasing size from 6 >> 20
+        for (i = START; i <= STOP; ++i) {
+            myFont = new Font("TimesRoman", Font.BOLD, i);
+            g.setFont(myFont);
+            g.drawString(message, HORIZ_POS, vertPos);
+            vertPos += INCREASE;
+        }
+    }
+
+    //running application
+    public static void main(String[] args) {
+        //defaults
+        JFrame frame = new JFrame();
+        frame.add(new JFontSizesPanel());
+        frame.setSize(360, 340);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
